4e26471b017c0698daa5f8fac1f2b83770e268d7
Jave T Lupango <jtlupango@gmail.com>
2021-01-22 11:20:39 +0800

    view

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About US</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ resources/views/contactus.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact US</title>
+</head>
+<body>
+
+</body>
+</html>
